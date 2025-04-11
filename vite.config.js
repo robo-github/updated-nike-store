fb3bfb4edd0fcc22172780ac6a7ca215146a8f53
anand_tp <anandpramod11@gmail.com>
2025-04-11 18:43:10 +0530

    Updated base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/nike-store",
   plugins: [react()],
-})
+});
