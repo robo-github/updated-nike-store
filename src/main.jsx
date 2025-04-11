b48d35b4091feb8cc9b751f2d0bce42679010e92
anand_tp <anandpramod11@gmail.com>
2025-04-11 19:08:32 +0530

    Updated HashRouter to StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
